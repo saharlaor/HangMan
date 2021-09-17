ef43ab80f0c86ddbd94fe0807714da3ae49ee368
saharoori <saharlaor@gmail.com>
2021-09-17 11:10:47 +0300

    Setup main file

---
+++ hangedMan.js
@@ -0,0 +1,19 @@
+/**
+* Hangman game written in JavaScript.
+*
+* The program generates a random word,
+* the user guesses letters,
+* if the word contains a letter its occurences are revealed,
+* o/w the user gets a strike.
+* The game continues until the user guesses the word or 10 strikes are reached.
+*
+* @link   https://github.com/saharlaor/hangman
+* @file   This file contains a full hangman game implementation.
+* @author SaharLaOr.
+* @since  1.0.0
+**/
+
+(function () {
+    'use strict';
+    
+})();
