314d4cd34cc735a658a4665986b7948529bcef1f
saharoori <saharlaor@gmail.com>
2021-09-17 12:59:02 +0300

    Basic functionality and documentation

--- hangedMan.js
+++ hangedMan.js
@@ -15,5 +15,58 @@
 
 (function () {
     'use strict';
-    
+
+    /**
+    * Generate and print an opening screen for the game.
+    *
+    * Print title screen in baloon letters.
+    *
+    * @since  1.0.0
+    **/
+    function openingScreen() {
+        
+    }
+
+    /**
+    * Handle the end of a game.
+    *
+    * Print a relevant message for the end of the game.
+    *
+    * @since  1.0.0
+    *
+    * @param {boolean}   isWon           Did the user win the game.
+    **/
+    function gameOver(isWon) {
+    }
+
+    /**
+    * Go through a user's turn.
+    *
+    * Let the user guess a letter, check it against the word.
+    *
+    * @since  1.0.0
+    *
+    **/
+    function userTurn() {
+        
+    }
+
+    /**
+    * Hanged man game.
+    *
+    * Generate a random word, print it opaquely (using '*').
+    * Let the user guess letters until he either wins or
+    * accumulate 10 wrong guesses.
+    *
+    * @since  1.0.0
+    **/
+    function startGame() {
+        // Generate a random word
+
+        // Loop until game over
+    }
+
+    // Start Game
+    openingScreen();
+    startGame();
 })();
